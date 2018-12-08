67a4d1beb0fe232cb7217dcab773ca022b01d5ee
Maru De Omana <jmdeomana@gmail.com>
2018-12-08 13:59:45 +0800

    helo

---
+++ app/src/main/java/com/example/maru/despro/User.java
@@ -0,0 +1,16 @@
+package com.example.maru.despro;
+
+public class User {
+    public String name, email,cp, age;
+
+    public User(){
+
+    }
+
+    public User(String name,String email, String cp, String age){
+        this.name= name;
+        this.email=email;
+        this.cp=cp;
+        this.age=age;
+    }
+}
