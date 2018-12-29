71bea6269d35fbc023e7fd4b388d544ce58728b1
mark fabon <mark.fabon29@gmail.com>
2018-12-29 23:41:34 +0800

    Okay na ata, pati nag sesave na, naayos na din yung firebase, yng graph na gumagalaw nasira puta naman

--- app/src/main/java/com/example/maru/despro/MonitorActivity.java
+++ app/src/main/java/com/example/maru/despro/MonitorActivity.java
@@ -1,6 +1,7 @@
 package com.example.maru.despro;
 
 import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.TransitionDrawable;
@@ -10,62 +11,105 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Random;
 
 public class MonitorActivity extends AppCompatActivity {
-    Button profile, linef,shit;
+    Button profile, linef, shit;
     ImageView temp;
-    LineGraphSeries<DataPoint> series;
+    double values[];
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
+    GraphView graph;
+    int i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_monitor);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
         final String uName = getIntent().getStringExtra("UserName");
-        profile = findViewById (R.id.button1);
-        linef = findViewById (R.id.button2);
-        temp=findViewById (R.id.temp);
-        shit=findViewById (R.id.shit);
-        double y,x;
-        Random random = new Random ();
-        x= random.nextInt(5);
+        profile = findViewById(R.id.button1);
+        linef = findViewById(R.id.button2);
+        temp = findViewById(R.id.temp);
+        shit = findViewById(R.id.shit);
 
-        shit.setOnClickListener (new View.OnClickListener () {
+        graph = findViewById(R.id.graph);
+
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        Viewport viewport = graph.getViewport();
+
+        viewport.setXAxisBoundsManual(true);
+
+        viewport.setMinX(0);
+        viewport.setMaxX(20);
+        viewport.setScrollable(true);
+
+
+
+        shit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((TransitionDrawable) temp.getDrawable()).startTransition (3000);
+                ((TransitionDrawable) temp.getDrawable()).startTransition(3000);
             }
         });
 
-        profile.setOnClickListener (new View.OnClickListener () {
+        profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (MonitorActivity.this, ProfileActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("UserName",uName);
-                startActivity (intent);
+                Intent intent = new Intent(MonitorActivity.this, ProfileActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("UserName", uName);
+                startActivity(intent);
             }
         });
 
-        linef.setOnClickListener (new View.OnClickListener () {
+        linef.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (MonitorActivity.this, LineFollowerActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("UserName",uName);
-                startActivity (intent);
+                Intent intent = new Intent(MonitorActivity.this, LineFollowerActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("UserName", uName);
+                startActivity(intent);
             }
         });
 
-        GraphView graph = findViewById (R.id.graph);
-        series= new LineGraphSeries<DataPoint> ();
-        for (int i=0; i<500; i++){
-            x=x+.1;
-            y= Math.sin(x);
-            series.appendData(new DataPoint (x,y), true,500);
-        }
-        graph.addSeries (series);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (i = 0; i<20; i++){
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                i = 0;
+
+            }
+        }).start();
+    }
+
+    private void addEntry(){
+         series.appendData(new DataPoint(lastX++,RANDOM.nextDouble()*20d),true,20);
+
+    }
+
 }
